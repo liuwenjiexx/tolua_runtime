2f4f0dbe016ed501725953e393cc71891001a1ad
liuwenjie <liuwenjiexx@sina.com>
2021-11-10 16:40:53 +0800

    优化批处理

--- windows_build_android_x32.sh
+++ windows_build_android_x32.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 source windows_android.sh
 
-cd ./luajit-2.1/src
-
+# Android armeabi-v7a
 NDK_ABI=armeabi-v7a
 NDK_TOOLCHAIN=arm-linux-androideabi-4.9
 
@@ -10,6 +9,8 @@ NDKVER=${NDK}/toolchains/${NDK_TOOLCHAIN}
 NDKP=${NDKVER}/prebuilt/windows-x86_64/bin/arm-linux-androideabi-
 NDKF="--sysroot ${NDK}/platforms/android-${NDK_API}/arch-arm" 
 NDKARCH="-march=armv7-a -mfloat-abi=softfp -Wl,--fix-cortex-a8"
+
+cd ./luajit-2.1/src
 
 make clean
 make HOST_CC="gcc -m32" CROSS=${NDKP} TARGET_SYS=Linux TARGET_FLAGS="${NDKF} ${NDKARCH}"
@@ -19,5 +20,25 @@ make clean
 cd ../../
 
 build ${NDK_ABI} ${NDK_TOOLCHAIN}
+
+
+# Android x86
+NDK_ABI=x86
+NDK_TOOLCHAIN=x86-4.9
+
+cd ./luajit-2.1/src
+
+NDKVER=${NDK}/toolchains/${NDK_TOOLCHAIN}
+NDKP=${NDKVER}/prebuilt/windows-x86_64/bin/i686-linux-android-
+NDKF="--sysroot ${NDK}/platforms/android-${NDK_API}/arch-x86"
+
+make clean
+make HOST_CC="gcc -m32" CROSS=${NDKP} TARGET_SYS=Linux TARGET_FLAGS="${NDKF}"
+cp ./libluajit.a ../../android/jni/libluajit.a
+make clean
+cd ../../
+
+build ${NDK_ABI} ${NDK_TOOLCHAIN}
+
 
 echo Successfully linked
